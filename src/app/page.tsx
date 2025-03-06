21e140311dae361be6b152773677e25fe87c97d8
AmirlanCode <zerobladeamir@gmail.com>
2025-03-06 18:08:08 +0500

    adding react icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
 "use client";
-
 import { motion } from "framer-motion";
-import { FaGithub } from "react-icons/fa";
-import { FaLinkedin } from "react-icons/fa";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+
 import Image from "next/image";
 
 const Home = () => {
